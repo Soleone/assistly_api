6c9f07926a1edc7d306618f2969aa92f38f040e3
Daniel Beauchamp <daniel.beauchamp@shopify.com>
2011-06-07 11:33:58 -0400

    Grab the credentials from a yaml file.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,27 +1,29 @@
 require File.dirname(__FILE__) + '/../lib/assistly'
 require 'test/unit'
+require 'yaml'
+ASSISTLY_CONFIG = YAML::load(File.open('assistly.yml'))[:production]
 
 class Test::Unit::TestCase
   
   # Enter credentials for testing below
 
   def subdomain
-    
+    ASSISTLY_CONFIG[:subdomain]
   end
   
   def consumer_key
-
+    ASSISTLY_CONFIG[:consumer_key]
   end
   
   def consumer_secret
-
+    ASSISTLY_CONFIG[:consumer_secret]
   end
   
   def access_token_key
-
+    ASSISTLY_CONFIG[:oauth_token]
   end
   
   def access_token_secret
-
+    ASSISTLY_CONFIG[:oauth_token_secret]
   end
 end
