eaf4a574f00b72103ddd7060c2b180ddbbbb4405
Dennis Theisen <dennis@shopify.com>
2012-09-10 13:09:00 -0400

    Revert "Set case.email on results if it was passed in for convenience" because it doesn't work when searching for multiple emails

--- lib/assistly/case.rb
+++ lib/assistly/case.rb
@@ -7,11 +7,8 @@ module Assistly
       # 1. get the last 100 tickets. If there are more than 100 in total:
       # 2. fetch the last page of tickets
       # 3. fetch the previous page to get any missing tickets to ensure +count+ number of cases get returned
-      #
-      # Also work around the fact that no case.email gets returned by setting it here if it was passed in
       def self.all_recent(options = {})
         count = options[:count] || 100
-        email = options[:email]
 
         old_cases = all(options.merge(:count => count))
 
@@ -19,7 +16,7 @@ module Assistly
         latest_page = (total / count.to_f).ceil
 
         if total <= count
-          return sorted_case_results(old_cases, total, count, latest_page, email)
+          return sorted_case_results(old_cases, total, count, latest_page)
         end
 
         latest_cases = all(options.merge(:page => latest_page))
@@ -29,15 +26,11 @@ module Assistly
           latest_cases = previous_cases[latest_cases.size..count] + latest_cases
         end
         
-        sorted_case_results(latest_cases, total, count, latest_page, email)
+        sorted_case_results(latest_cases, total, count, latest_page)
       end
 
       # TODO: UGLY HACK, PLEASE REFACTOR RESULT TO ACCEPT AN ARRAY OF OBJECTS 
-      def self.sorted_case_results(cases, total, count, latest_page, email = nil)
-        if email
-          cases.each{ |ticket| ticket.email = email }
-        end
-
+      def self.sorted_case_results(cases, total, count, latest_page)
         cases.sort!{|a, b| b.created_at <=> a.created_at}
 
         meta_class = (class << cases; self; end)
